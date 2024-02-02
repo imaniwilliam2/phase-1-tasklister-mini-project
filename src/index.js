c1d1859ad10c7dc4c88186445291721cfcc63936
imaniwilliam2 <154285022+imaniwilliam2@users.noreply.github.com>
2024-02-01 19:47:37 -0500

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    doToDo(e.target.new_task_description.value)
+    form.reset()
+
+
+  })
+
+
 });
+
+function doToDo(todo) {
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', deleteToDo)
+  btn.textContent = 'x'
+  p.textContent = `${todo} `
+  p.appendChild(btn)
+  console.log(p)
+  document.querySelector('#tasks').appendChild(p)
+
+}
+
+function deleteToDo(e){
+  e.target.parentNode.remove()
+}
